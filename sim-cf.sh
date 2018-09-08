a5d88beea50d34f74291a1acadbc8bef36667808
williamsungz <williamsungz@gmail.com>
2018-09-08 17:25:19 +0800

    Minimized data set

--- sim-cf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-yhrun -N 4 -n 64 -p MEM_128 -c 1 ./grape-engine --command_file ./sim-cf.gql --v 2  --deserialize true
--- sim-t.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yhrun -N 4 -n 64 -p MEM_128 -c 1 ./grape-engine --command_file ./sim-t.gql --v 2 # --deserialize true
-
